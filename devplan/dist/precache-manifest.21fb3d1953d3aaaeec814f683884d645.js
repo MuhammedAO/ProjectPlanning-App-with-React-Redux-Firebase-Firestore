d3ac966be7eb47c1653c4973e4df5ab33b1c9b0c
Muhammed ogunsanya <mspeaks910@gmail.com>
2019-10-06 02:12:47 +0100

    devPlan completed

---
+++ devplan/dist/precache-manifest.21fb3d1953d3aaaeec814f683884d645.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "47bca1cf4b1283b5836c8b0d7f1318fd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "316e82c3dbc5e15ef6e5",
+    "url": "/static/css/main.be434b3a.chunk.css"
+  },
+  {
+    "revision": "2b12f6ab8058f4e50ecc",
+    "url": "/static/js/2.19d6fbe4.chunk.js"
+  },
+  {
+    "revision": "316e82c3dbc5e15ef6e5",
+    "url": "/static/js/main.db260ce1.chunk.js"
+  },
+  {
+    "revision": "1cda1da5677247dbe19d",
+    "url": "/static/js/runtime-main.df942983.js"
+  }
+]);
---
+++ devplan/src/store/reducers/authReducer.js
@@ -0,0 +1,40 @@
+const InitState = {
+    authError: null
+}
+
+const authReducer = (state=InitState, action) =>{
+    switch(action.type) {
+        case 'LOGIN_ERROR':
+            console.log('login failed');
+        return {
+            ...state,
+            authError:'Login failed'
+        }
+        case 'LOGIN_SUCCESS':
+        console.log('login success');
+        return {
+            ...state,
+            authError:null
+        }
+        case 'SIGNOUT_SUCCESS':
+            console.log('signout success')
+            return state;
+        case 'SIGNUP_SUCCESS':
+            console.log('signup successful');
+            return {
+                ...state,
+                authError: null
+            }  
+        case 'SIGNUP_ERROR':
+           console.log('signup failed');
+           return {
+               ...state,
+            authError: action.err.message
+           }       
+            
+        default:
+        return state;
+    }
+}
+
+export default authReducer 
